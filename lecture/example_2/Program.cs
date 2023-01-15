268612c5ff28ec10c1f604762166ed91cae67e90
zhoma96 <zhchyrmashuulu@gmail.com>
2023-01-15 21:00:02 +0300

    Example with finding element in array

---
+++ lecture/example_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1, 4, 5, 11, 15, 111, 19, 20, 111};
+int n = array.Length;
+int find = 111;
+int index = 0;
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
